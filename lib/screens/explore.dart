2175352faf3dcbbdbc43425e9450a846fe0e920f
Parsa Sharifi <sharifidev@outlook.com>
2022-12-12 01:09:47 +0330

    add basic Widgets to this screens

---
+++ lib/screens/explore.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../widgets/w_header.dart';
+import '../widgets/w_inputs.dart';
+
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    bool hasNotification = true;
+    final myController = TextEditingController();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            getHomeHeader(context, hasNotification, myController),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Return Header/AppBar Widget
+  Widget getHomeHeader(context, hasNotification, myController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Header(
+              hasNotification: hasNotification,
+              title: SvgPicture.asset(
+                'assets/images/img_explore/logo.svg',
+              ),
+              icon1: SvgPicture.asset(
+                'assets/icons/icn_appbar/notification_dot.svg',
+              ),
+              icon2: SvgPicture.asset(
+                'assets/icons/icn_appbar/notification.svg',
+              ),
+              buttomClick: () {},
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SearchBar(
+                controller: myController,
+                onChanged: (value) {
+                  print(value);
+                },
+              ),
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 14,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/profile.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../widgets/w_header.dart';
+import '../widgets/w_inputs.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    bool hasNotification = true;
+    final myController = TextEditingController();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            getHomeHeader(context, hasNotification, myController),
+            //
+            //
+            //
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    //
+                    /* محتوای کل این صفحه اینجا بنویس  */
+                    //
+                  ],
+                ),
+              ),
+            ),
+            //
+            //
+            //
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Return Header/AppBar Widget
+  Widget getHomeHeader(context, hasNotification, myController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Header(
+              hasNotification: hasNotification,
+              title: SvgPicture.asset(
+                'assets/images/img_profile/logo.svg',
+              ),
+              icon1: SvgPicture.asset(
+                'assets/icons/icn_appbar/notification_dot.svg',
+              ),
+              icon2: SvgPicture.asset(
+                'assets/icons/icn_appbar/notification.svg',
+              ),
+              buttomClick: () {},
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 28,
+          )
+        ],
+      ),
+    );
+  }
+}
