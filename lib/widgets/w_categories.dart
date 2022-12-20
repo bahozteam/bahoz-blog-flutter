495a2b267658d5de1bade9c808a31bd9bfeee236
Parsa Sharifi <sharifidev@outlook.com>
2022-12-20 19:18:38 +0330

    add lists to categories widget and tiny changes

--- lib/widgets/w_categories.dart
+++ lib/widgets/w_categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/list_model.dart';
 
 class Categories extends StatefulWidget {
   const Categories({super.key});
@@ -21,97 +22,69 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            Container(
-              margin: EdgeInsets.only(right: 28),
-              height: 36,
-              width: double.infinity,
-              child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: items.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: ((context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        current = index;
-                      });
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 300),
-                      // curve: Curves.slowMiddle,
-                      margin: EdgeInsets.only(left: 11),
-                      // height: 36,
-                      decoration: BoxDecoration(
-                        color: current == index
-                            ? Color(0xFF159F91)
-                            : Color(0xFFEBEBEB),
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 14),
-                        child: Center(
-                          child: Text(
-                            items[index],
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: current == index
-                                  ? Colors.white
-                                  : Colors.black,
-                            ),
+      // width: double.infinity,
+      // height: double.infinity,
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 20),
+            height: 36,
+            width: double.infinity,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: items.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: ((context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      current = index;
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 200),
+                    // curve: Curves.slowMiddle,
+                    margin: EdgeInsets.only(left: 11),
+                    // height: 36,
+                    decoration: BoxDecoration(
+                      color: current == index
+                          ? Color(0xFF2E8EFF)
+                          : Color(0xFFEBEBEB),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 14),
+                      child: Center(
+                        child: Text(
+                          items[index],
+                          style: TextStyle(
+                            fontSize: 14,
+                            color:
+                                current == index ? Colors.white : Colors.black,
                           ),
                         ),
                       ),
                     ),
-                  );
-                }),
-              ),
+                  ),
+                );
+              }),
             ),
-            SizedBox(height: 24),
-            getList().elementAt(current),
-          ],
-        ),
+          ),
+          SizedBox(height: 24),
+          getList().elementAt(current),
+        ],
       ),
     );
   }
 
   List<Widget> getList() {
     return <Widget>[
-      Container(
-        width: double.infinity,
-        height: 1000,
-        color: Colors.amber[300],
-      ),
-      Container(
-        width: double.infinity,
-        height: 1000,
-        color: Colors.orange[300],
-      ),
-      Container(
-        width: double.infinity,
-        height: 1000,
-        color: Colors.purple[300],
-      ),
-      Container(
-        width: double.infinity,
-        height: 1000,
-        color: Colors.pink[300],
-      ),
-      Container(
-        width: double.infinity,
-        height: 1000,
-        color: Colors.blueAccent[300],
-      ),
-      Container(
-        width: double.infinity,
-        height: 1000,
-        color: Colors.teal[300],
-      ),
+      RecommendedPosts(),
+      RecommendedPosts(),
+      RecommendedPosts(),
+      RecommendedPosts(),
+      RecommendedPosts(),
+      RecommendedPosts(),
     ];
   }
 }
