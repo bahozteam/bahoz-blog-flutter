e1097ca579a40a23f742307c2ee60a1499102270
Parsa Nik <94043228+Parsa-Nik@users.noreply.github.com>
2022-12-21 15:10:02 +0330

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
+import '../models/profile_model.dart';
 import '../widgets/w_header.dart';
+
+//ParsaNik
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -30,7 +32,96 @@ class _ProfileScreenState extends State<ProfileScreen> {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    /* محتوای کل این صفحه اینجا بنویس  */
+                    Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/94043228?v=4'),
+                            radius: 30,
+                          ),
+                          title: Padding(
+                            padding: const EdgeInsets.only(bottom: 6.0),
+                            child: Text('پارسا نیک سیرت'),
+                          ),
+                          subtitle: Text('فلاتر دولوپر'),
+                        ),
+                        //
+                        //
+                        SizedBox(
+                          height: 12,
+                        ),
+                        //
+                        //
+                        //
+                        Container(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 40,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '۱۲ دنبال شونده',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 60,
+                                child: VerticalDivider(
+                                  width: 30,
+                                  thickness: 2,
+                                  indent: 20,
+                                  endIndent: 20,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '۱۲ دنبال کننده',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 60,
+                                child: VerticalDivider(
+                                  width: 30,
+                                  thickness: 2,
+                                  indent: 20,
+                                  endIndent: 20,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '۱۲ مقالات',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        //
+                        //
+                        TitleOfSections(
+                          text: 'پیش نویس',
+                          icon: 'assets/icons/icn_main/slidervertical.svg',
+                        ),
+                        SizedBox(height: 18),
+
+                        /* get Recommended Blog Posts */
+                        Draft(),
+                        TitleOfSections(
+                          text: 'منتشر شده',
+                          icon: 'assets/icons/icn_main/slidervertical.svg',
+                        ),
+                        SizedBox(height: 18),
+
+                        /* get Recommended Blog Posts */
+                        RecommendedPosts(),
+                      ],
+                    ),
                   ],
                 ),
               ),
@@ -40,6 +131,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             //
           ],
         ),
+        drawer: Profile_drawer(),
       ),
     );
   }
